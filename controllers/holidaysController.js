fc9b9e559b2319b81a5bda5a653e6fd7a57eada6
wanxuanh <wanxuan.ho@gmail.com>
2022-04-02 15:32:20 +0800

    post

--- controllers/holidaysController.js
+++ controllers/holidaysController.js
@@ -18,8 +18,30 @@ router.get("/seed", async (req, res) => {
   res.json(holidays)
 })
 
+
 router.get('/', (req, res) => {
   res.send('holidays');
 })
+
+//? CREATE ROUTE
+
+//? need to check, try catch is to check for "", send a success/failure
+router.post("/", async (req,res) =>{
+  //* extract the data from req.body
+  console.log("body", req.body)
+  try {
+    const createdHoliday = await Holiday.create(req.body);
+    res.status(200).send(createdHoliday);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  };  req.body = { name: "My birthday", likes:888 }
+  //* save the data using moongoose into database
+
+  Holiday.save(req.body);
+
+  res.send(); /// ???? 
+
+});
+
 
 module.exports = router;
