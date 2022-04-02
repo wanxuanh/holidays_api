833249e983a9a52c4aa16221b4dc7deb4f604e10
wanxuanh <wanxuan.ho@gmail.com>
2022-04-02 12:41:41 +0800

    added json

--- controllers/holidaysController.js
+++ controllers/holidaysController.js
@@ -4,16 +4,18 @@ const Holiday = require("../models/holidays")
 const router = express.Router();
 
 router.get("/seed", async (req, res) => {
-  await Holiday.deleteMany({});
-  await Holiday.insertMany([
+
+  const holidays = [
     {
       name: "New Year's Day",
     },
     {
       name: "Good Friday",
     }
-  ])
-  res.send("holidays seeded")
+  ]
+  await Holiday.deleteMany({});
+  await Holiday.insertMany(holidays)
+  res.json(holidays)
 })
 
 router.get('/', (req, res) => {
